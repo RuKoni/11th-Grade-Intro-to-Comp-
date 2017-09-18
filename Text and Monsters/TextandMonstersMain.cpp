41e8e93baffd70b3fafc571ffa87221ac695d576
Hexas <31778802+BringzStorm@users.noreply.github.com>
2017-09-18 09:51:12 -0500

    Create TextandMonstersMain.cpp

---
+++ Text and Monsters/TextandMonstersMain.cpp
@@ -0,0 +1,47 @@
+//Nichol He
+
+#include "stdlib.h"
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <iomanip>
+
+int interface ();
+
+int main () {
+
+string gameStart, name;
+int HP, MP, HPMax, MPMax;
+int Atk, S.Atk, Def, S.Def, Agi, Eva, Acc, Luck, Crit;
+
+cout << "Welcome to Text and Monsters! Would you like to play?" << endl;
+cout << "\t Yes or No" << endl;
+  cin >> gameStart;
+  
+  if (gameStart == "yes" || gameStart == "Yes") {
+    
+  
+  }
+  else {
+    cout << endl;
+    cout << "See you next time!" << endl;
+  }
+  
+
+
+}
+
+int interface () {
+cout << name << endl;
+cout << "HP = ( " << HP << "/" << HPMax << " )" << endl;
+cout << "MP = ( " << MP << "/" << MPMax << " )" << endl;
+cout << endl << endl << endl << endl << endl;
+cout << "====================" << endl;
+cout << "||" << setw(8) << "||" << endl;
+cout << "||" << setw(8) << "||" << endl;
+cout << "||" << setw(8) << "||" << endl;
+cout << "||" << setw(8) << "||" << endl;
+cout << "||" << setw(8) << "||" << endl;
+cout << "||" << setw(8) << "||" << endl;
+cout << "===================" << endl;
+}
