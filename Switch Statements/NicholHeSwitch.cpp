09ea3fe8f9b21b66c0c2954d89ea1731c43b0462
Hexas <31778802+BringzStorm@users.noreply.github.com>
2017-09-19 08:14:12 +0900

    Update NicholHeSwitch.cpp

--- Switch Statements/NicholHeSwitch.cpp
+++ Switch Statements/NicholHeSwitch.cpp
@@ -1,55 +1,74 @@
-// NicholHeSwitch.cpp : Defines the entry point for the console application.
-//
+// NicholHeSWITCH.cpp : Defines the entry point for the console application.
+// Nichol He
+
+/*
+Program that takes in 2 numbers and gives an option for operations. Then performs the operation on inputted Numbers.
+1) Addition
+2) Subtraction
+3) Multiplication
+4) Division
+5) Average
+6) Modulo
+*/
 
 #include "stdafx.h"
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-	int Num1, Num2, operation, result;
-
+	//list Variables
+	double result, Num1, Num2;
+	int operation;
+		//Asks for input and gives option screen
 	cout << "Welcome! Please enter your first Number." << endl;
-		cin >> Num1;
+	cin >> Num1;
+	cout << endl;
 	cout << "Please enter your second Number." << endl;
-		cin >> Num2;
+	cin >> Num2;
+	cout << endl;
 	cout << "Please enter the operation you would like to perform." << endl;
-	cout << "Enter 1 for Addition" << endl;
-	cout << "Enter 2 for Subtraction" << endl;
-	cout << "Enter 3 for Multiplication" << endl;
-	cout << "Enter 4 for Division" << endl;
-	cout << "Enter 5 for Average" << endl;
-	cout << "Enter 6 for Modulus" << endl;
-		cin >> operation;
+	cout << "\t 1 for Addition" << endl;
+	cout << "\t 2 for Subtraction" << endl;
+	cout << "\t 3 for Multiplication" << endl;
+	cout << "\t 4 for Division" << endl;
+	cout << "\t 5 for Average" << endl;
+	cout << "\t 6 for Modulus" << endl;
+	cin >> operation;
+	cout << endl;
+		//switch statement to evaluate operation choice and outputs result
+	switch (operation) {
+	default : 
+		cout << "Invalid Option" << endl;
+		break;
+	case (1):
+		result = Num1 + Num2;
+		cout << "The sum of your numbers is " << result << endl;
+		break;
+	case (2):
+		result = Num1 - Num2;
+		cout << "The difference of your numbers is " << result << endl;
+		break;
+	case (3):
+		result = Num1 * Num2;
+		cout << "The product of your numbers is " << result << endl;
+		break;
+	case (4):
+		result = Num1 / Num2;
+		cout << "The quotient of your numbers is " << result << endl;
+		break;
+	case (5):
+		result = (Num1 + Num2) / 2;
+		cout << "The average of your numbers is " << result << endl;
+		break;
+	case (6):
+		result = fmod(Num1, Num2);
+		cout << "The modolus of your numbers is " << result << endl;
+		break;
+	}
 
-	cin >> operation; 
-
-		switch (operation) {
-			case (1) :
-				result = Num1 + Num2;
-				cout << "The sum of your numbers is " << result << endl;
-				break;
-			case (2) :
-				result = Num1 - Num2;
-				cout << "The difference of your numbers is " << result << endl;
-				break;
-			case (3) :
-				result = Num1 * Num2;
-				cout << "The product of your numbers is " << result << endl;
-				break;
-			case (4) :
-				result = Num1 / Num2;
-				cout << "The quotient of your numbers is " << result << endl;
-				break;
-			case (5) :
-
-				break;
-			case (6) :
-
-				break;
-		}
 	return 0;
 }
+
 
